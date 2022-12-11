aa2e208ea08dc1b055d3a494ee90adec8deb14aa
Reebey <reebey@wp.pl>
2022-12-11 22:19:00 +0100

    basic scans

--- mobile/lib/service/text_scanner_service.dart
+++ mobile/lib/service/text_scanner_service.dart
@@ -2,6 +2,7 @@ import 'dart:math';
 import 'dart:ui';
 
 import 'package:google_mlkit_text_recognition/google_mlkit_text_recognition.dart';
+import 'package:string_similarity/string_similarity.dart';
 
 class TextScanner {
   static Future<void> printImageText(String path) async {
@@ -10,11 +11,54 @@ class TextScanner {
     final RecognizedText recognizedText =
         await textRecognizer.processImage(inputImage);
 
-    String text = recognizedText.text;
+    // String text = recognizedText.text;
+    String header = "";
+    bool wasHeader = false;
+    bool wasBody = false;
+    Map<String, String> lines = {};
+    String lastLine = "";
+    int lineIndex = 0;
+    String footer = "";
+    double d = 1;
+
+    TextBlock titleBlock = recognizedText.blocks
+        .firstWhere((element) => element.text.contains('PARAGON FISKALNY'));
+    recognizedText.blocks
+        .sort((a, b) => a.boundingBox.top.compareTo(b.boundingBox.top));
     for (TextBlock block in recognizedText.blocks) {
-      print('=================================================');
-      print('${block.text}');
+      if (wasBody) {
+        footer += block.text;
+      } else if (wasHeader) {
+        if (block.lines.any((element) =>
+                StringSimilarity.compareTwoStrings(
+                    element.text, 'Sprzedaz opodatkowana') >
+                0.3) ||
+            block.text.contains('SPRZEDAZ OPODATKOWANA')) {
+          wasBody = true;
+          footer += block.text;
+          continue;
+        }
+
+        if (lineIndex % 2 == 1) {
+          lines[lastLine] = block.text;
+        }
+
+        lastLine = block.text;
+        lineIndex++;
+      } else {
+        header += block.text;
+        if (block.boundingBox.top > titleBlock.boundingBox.top) {
+          // print(
+          //     '${block.boundingBox.bottom} > ${titleBlock.boundingBox.bottom}');
+          wasHeader = true;
+        }
+      }
+
+      // print(block.boundingBox.toString());
+      // print(block.cornerPoints.toString());
+      // print('[${block.boundingBox.}]');
       print(' ............. ');
+      print('${block.boundingBox}');
 
       // print(block.toString());
       // final Rect rect = block.boundingBox;
@@ -23,15 +67,20 @@ class TextScanner {
       // final List<String> languages = block.recognizedLanguages;
 
       for (TextLine line in block.lines) {
-        // print(' - ');
-
         // Same getters as TextBlock
-        // print('> ${line.text}');
+        print('> ${line.text}');
         // for (TextElement element in line.elements) {
         //   print('Element ${element.text}');
-        // }
       }
+      // }
     }
+    print(header);
+    print(' ............. ');
+    lines.forEach((key, value) {
+      print('$key\t----\t$value');
+    });
+    print(' ............. ');
+    print(footer);
 
     textRecognizer.close();
   }
