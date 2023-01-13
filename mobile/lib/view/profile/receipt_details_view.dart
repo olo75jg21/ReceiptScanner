4622defe3316c54441a09e798ee7cd71788b1178
olo75jg21 <olo75jg12@gmail.com>
2023-01-13 15:49:17 +0100

    add rendering receipt items

--- mobile/lib/view/profile/receipt_details_view.dart
+++ mobile/lib/view/profile/receipt_details_view.dart
@@ -1,11 +1,29 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 import 'package:mobile/product/model/receipt.dart';
+import 'package:path/path.dart';
 import '../../core/constant/app_color.dart';
 
 class ReceiptDetailView extends StatelessWidget {
   final Receipt receipt;
   ReceiptDetailView({Key? key, required this.receipt}) : super(key: key);
+
+  Widget renderReceiptItems() {
+    return Column(
+      children: <Widget>[
+        for (var item in receipt.receiptItems!)
+          Card(
+            child: Text(
+              item.name!,
+              style: TextStyle(fontSize: 25.0),
+            ),
+          ),
+      ],
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,8 +32,6 @@ class ReceiptDetailView extends StatelessWidget {
           title: Text(receipt.shop.toString().toUpperCase()),
           backgroundColor: AppColors.loginColor,
         ),
-        body: Center(
-          child: Text(receipt.price.toString()),
-        ));
+        body: Center(child: renderReceiptItems()));
   }
 }
