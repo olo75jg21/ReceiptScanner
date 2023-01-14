47d13cb6c51c4b1eed6b1c525a5b96546c7514d6
Jakub Olcha <olo75jg12@gmail.com>
2023-01-14 02:58:51 +0100

    performing deletion of receipt item

--- mobile/lib/view/profile/receipt_details_view.dart
+++ mobile/lib/view/profile/receipt_details_view.dart
@@ -3,7 +3,7 @@ import 'package:flutter/widgets.dart';
 
 import 'package:mobile/product/model/receipt.dart';
 import 'package:mobile/product/model/receipt_item.dart';
-import 'package:path/path.dart';
+// import 'package:path/path.dart';
 import '../../core/constant/app_color.dart';
 
 class ReceiptDetailView extends StatefulWidget {
@@ -47,14 +47,8 @@ class _ReceiptDetailViewState extends State<ReceiptDetailView> {
 
   Card topArea() => Card(
         margin: const EdgeInsets.all(10.0),
-        elevation: 1.0,
-        shape:
-            RoundedRectangleBorder(borderRadius: BorderRadius.circular(50.0)),
         child: Container(
-            decoration: const BoxDecoration(
-                gradient: RadialGradient(
-                    colors: [Color(0xFF015FFF), Color(0xFF015FFF)])),
-            // color: Color(0xFF015FFF),
+            color: const Color.fromARGB(255, 235, 232, 232),
             child: Column(
               children: <Widget>[
                 Row(
@@ -65,68 +59,100 @@ class _ReceiptDetailViewState extends State<ReceiptDetailView> {
                           left: 10.0, right: 10.0, top: 10.0, bottom: 15.0),
                       child: Text(_receipt.shop.toString(),
                           style: const TextStyle(
-                              color: Colors.white,
-                              fontSize: 26.0,
-                              fontWeight: FontWeight.w400)),
+                              fontSize: 26.0, fontWeight: FontWeight.w400)),
                     ),
                     Container(
                       margin: const EdgeInsets.only(
                           top: 10.0, bottom: 15.0, right: 10.0),
                       child: Text(
                           '${_receiptCreatedAt.day.toString()}-${_receiptCreatedAt.month.toString()}-${_receiptCreatedAt.year.toString()}',
-                          style: const TextStyle(
-                              color: Colors.white, fontSize: 17.0)),
+                          style: const TextStyle(fontSize: 17.0)),
                     )
                   ],
                 ),
-                Center(
-                  child: Padding(
-                    padding: const EdgeInsets.all(1.0),
-                    child: Text('${_receipt.price} zl',
-                        style: const TextStyle(
-                            color: Colors.white, fontSize: 24.0)),
-                  ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: <Widget>[
+                    Container(
+                      padding: const EdgeInsets.all(1.0),
+                      margin: const EdgeInsets.only(left: 10.0),
+                      child: Text('Total: ${_receipt.price} zl',
+                          style: const TextStyle(fontSize: 22.0)),
+                    )
+                  ],
                 ),
                 const SizedBox(height: 12.0),
               ],
             )),
       );
 
-  Container accountItems(ReceiptItem receiptItem,
+  GestureDetector receiptItems(ReceiptItem receiptItem,
           {Color oddColour = Colors.white}) =>
-      Container(
-        decoration: BoxDecoration(color: oddColour),
-        padding: const EdgeInsets.only(
-            top: 20.0, bottom: 20.0, left: 5.0, right: 5.0),
-        child: Column(
-          children: <Widget>[
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      GestureDetector(
+          onLongPress: () {
+            showModalBottomSheet(
+                context: context,
+                builder: (context) {
+                  return Container(
+                    alignment: Alignment.center,
+                    padding: EdgeInsets.all(16.0),
+                    height: 60,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        IconButton(
+                          icon: const Icon(Icons.cancel),
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                        ),
+                        IconButton(
+                          icon: const Icon(Icons.delete),
+                          color: Colors.red,
+                          onPressed: () {
+                            receiptItem.deleteReceiptItem(
+                                _receipt.sId.toString(),
+                                receiptItem.sId.toString());
+                          },
+                        ),
+                      ],
+                    ),
+                  );
+                });
+          },
+          child: Container(
+            decoration: BoxDecoration(color: oddColour),
+            padding: const EdgeInsets.only(
+                top: 20.0, bottom: 20.0, left: 5.0, right: 5.0),
+            child: Column(
               children: <Widget>[
-                Text(receiptItem.name.toString(),
-                    style: const TextStyle(
-                        fontSize: 18.0, fontWeight: FontWeight.w500)),
-                Text(
-                    "Total:  ${receiptItem.priceInvidual! * receiptItem.amount!} zl",
-                    style: const TextStyle(
-                        fontSize: 16.0, fontWeight: FontWeight.w500))
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text(receiptItem.name.toString(),
+                        style: const TextStyle(
+                            fontSize: 18.0, fontWeight: FontWeight.w500)),
+                    Text(
+                        "Total:  ${receiptItem.priceInvidual! * receiptItem.amount!} zl",
+                        style: const TextStyle(
+                            fontSize: 16.0, fontWeight: FontWeight.w500))
+                  ],
+                ),
+                const SizedBox(
+                  height: 10.0,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text('${receiptItem.amount} ${receiptItem.unit}',
+                        style: const TextStyle(fontSize: 14.0)),
+                    Text('${receiptItem.priceInvidual} zl/${receiptItem.unit}',
+                        style: const TextStyle(fontSize: 14.0))
+                  ],
+                ),
               ],
             ),
-            const SizedBox(
-              height: 10.0,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                Text('${receiptItem.amount} ${receiptItem.unit}',
-                    style: const TextStyle(fontSize: 14.0)),
-                Text('${receiptItem.priceInvidual} zl/${receiptItem.unit}',
-                    style: const TextStyle(fontSize: 14.0))
-              ],
-            ),
-          ],
-        ),
-      );
+          ));
 
   displayReceiptItemsList() {
     return Container(
@@ -134,7 +160,7 @@ class _ReceiptDetailViewState extends State<ReceiptDetailView> {
       child: Column(
         children: <Widget>[
           for (var receiptItem in _receipt.receiptItems!.asMap().entries)
-            accountItems(receiptItem.value,
+            receiptItems(receiptItem.value,
                 oddColour: receiptItem.key % 2 == 0
                     ? const Color(0xFFF7F7F9)
                     : Colors.white)
