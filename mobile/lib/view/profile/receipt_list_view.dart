c9f7347320ddaa4e124d89148306417ac3246645
Jakub Olcha <olo75jg12@gmail.com>
2023-01-14 06:28:19 +0100

    fixing styling, change the way of entering receipts details

--- mobile/lib/view/profile/receipt_list_view.dart
+++ mobile/lib/view/profile/receipt_list_view.dart
@@ -22,17 +22,15 @@ class _ReceiptListState extends State<ReceiptListView> {
   }
 
   Text renderCreatedAtDate(DateTime date) =>
-      Text('${date.day}-${date.month}-${date.year}');
+      Text('${date.day}-${date.month}-${date.year}',
+          style: const TextStyle(fontSize: 14, fontWeight: FontWeight.w900));
 
   @override
   Widget build(BuildContext context) {
     // add jwt check
 
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Your receipts'),
-        backgroundColor: AppColors.loginColor,
-      ),
+      backgroundColor: const Color.fromARGB(255, 236, 234, 234),
       body: FutureBuilder<List<Receipt>>(
         future: futureReceipts,
         builder: (context, snapshot) {
@@ -40,20 +38,14 @@ class _ReceiptListState extends State<ReceiptListView> {
             return ListView.builder(
               itemCount: snapshot.data!.length,
               itemBuilder: (_, index) => InkWell(
-                onTap: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => ReceiptDetailView(
-                              receipt: snapshot.data![index])));
-                },
                 child: Container(
                   margin:
-                      const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-                  padding: const EdgeInsets.all(20.0),
+                      const EdgeInsets.symmetric(horizontal: 10, vertical: 4),
+                  padding: const EdgeInsets.only(top: 5, left: 10, right: 10),
                   decoration: BoxDecoration(
                     // color: const Color(0xff97FFFF),
-                    border: Border.all(color: AppColors.loginColor, width: 2),
+                    // border: Border.all(color: AppColors.loginColor, width: 2),
+                    color: const Color.fromARGB(255, 215, 222, 224),
                     borderRadius: BorderRadius.circular(15.0),
                   ),
                   child: Column(
@@ -61,72 +53,114 @@ class _ReceiptListState extends State<ReceiptListView> {
                     crossAxisAlignment: CrossAxisAlignment.stretch,
                     children: [
                       Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: [
-                          Text(
-                            snapshot.data![index].shop.toString().toUpperCase(),
-                            style: const TextStyle(
-                              fontSize: 20.0,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
+                          const SizedBox(width: 45),
                           renderCreatedAtDate(DateTime.parse(
                               snapshot.data![index].createdAt.toString()))
                         ],
                       ),
-                      const SizedBox(height: 20),
                       Row(
                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: [
-                          Text(
-                            "${snapshot.data![index].price} zl",
-                            style: const TextStyle(
-                                fontSize: 18, fontWeight: FontWeight.bold),
+                          Column(children: [
+                            Row(
+                              children: [
+                                const Icon(
+                                  Icons.shopping_cart,
+                                  size: 35,
+                                ),
+                                const SizedBox(width: 5),
+                                Text(
+                                  snapshot.data![index].shop
+                                      .toString()
+                                      .toUpperCase(),
+                                  style: const TextStyle(
+                                    fontSize: 18.0,
+                                    fontWeight: FontWeight.w900,
+                                  ),
+                                ),
+                                Text(
+                                  " - ${snapshot.data![index].price} PLN",
+                                  style: const TextStyle(
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.w900),
+                                ),
+                              ],
+                            ),
+                          ]),
+                          Column(
+                            children: [
+                              Row(
+                                children: [
+                                  IconButton(
+                                    onPressed: () {
+                                      showModalBottomSheet(
+                                          context: context,
+                                          builder: (context) {
+                                            return Container(
+                                              alignment: Alignment.center,
+                                              padding:
+                                                  const EdgeInsets.all(16.0),
+                                              height: 60,
+                                              child: Row(
+                                                mainAxisAlignment:
+                                                    MainAxisAlignment.center,
+                                                children: <Widget>[
+                                                  IconButton(
+                                                    icon: const Icon(
+                                                        Icons.cancel),
+                                                    onPressed: () {
+                                                      Navigator.pop(context);
+                                                    },
+                                                  ),
+                                                  IconButton(
+                                                    icon: const Icon(
+                                                        Icons.delete),
+                                                    color: Colors.red,
+                                                    onPressed: () {
+                                                      Receipt.deleteReceipt(
+                                                          snapshot
+                                                              .data![index].sId
+                                                              .toString());
+
+                                                      Navigator.pop(context);
+
+                                                      setState(() {
+                                                        futureReceipts = Receipt
+                                                            .fetchReceipts();
+                                                      });
+                                                    },
+                                                  ),
+                                                ],
+                                              ),
+                                            );
+                                          });
+                                    },
+                                    icon: const Icon(
+                                      Icons.delete,
+                                      size: 30,
+                                    ),
+                                  ),
+                                  IconButton(
+                                    onPressed: () {
+                                      Navigator.push(
+                                          context,
+                                          MaterialPageRoute(
+                                              builder: (context) =>
+                                                  ReceiptDetailView(
+                                                      receipt: snapshot
+                                                          .data![index])));
+                                    },
+                                    icon: const Icon(Icons.arrow_forward),
+                                    iconSize: 30,
+                                  )
+                                ],
+                              )
+                            ],
                           ),
-                          IconButton(
-                              onPressed: () {
-                                showModalBottomSheet(
-                                    context: context,
-                                    builder: (context) {
-                                      return Container(
-                                        alignment: Alignment.center,
-                                        padding: const EdgeInsets.all(16.0),
-                                        height: 60,
-                                        child: Row(
-                                          mainAxisAlignment:
-                                              MainAxisAlignment.center,
-                                          children: <Widget>[
-                                            IconButton(
-                                              icon: const Icon(Icons.cancel),
-                                              onPressed: () {
-                                                Navigator.pop(context);
-                                              },
-                                            ),
-                                            IconButton(
-                                              icon: const Icon(Icons.delete),
-                                              color: Colors.red,
-                                              onPressed: () {
-                                                Receipt.deleteReceipt(snapshot
-                                                    .data![index].sId
-                                                    .toString());
-
-                                                Navigator.pop(context);
-
-                                                setState(() {
-                                                  futureReceipts =
-                                                      Receipt.fetchReceipts();
-                                                });
-                                              },
-                                            ),
-                                          ],
-                                        ),
-                                      );
-                                    });
-                              },
-                              icon: const Icon(Icons.delete),
-                              color: Colors.red)
                         ],
                       ),
+                      const SizedBox(height: 5),
                     ],
                   ),
                 ),
